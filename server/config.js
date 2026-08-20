ba822414726a19563c2e1dec7f644e20329e1aad
beauvais23 <beauvais23@gmail.com>
2026-08-20 18:31:56 +0000

    Update ManufacturingOS supervisor URL for production

--- server/config.js
+++ server/config.js
@@ -6,6 +6,6 @@ module.exports = {
 
     supervisorUrl:
         process.env.SUPERVISOR_URL ||
-        "https://cautious-spork-qvv7j7rw46xghwrx-5173.app.github.dev/supervisor"
+        "https://manufacturingos-frontend-production.up.railway.app/supervisor"
 
 };
